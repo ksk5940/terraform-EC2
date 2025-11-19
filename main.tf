1cc47b5b70b61e0df90da89c44fb7ec5041ce3bd
Sreekanth K <ksk5940@gmail.com>
2025-11-19 16:10:07 +0530

    Refactor main.tf to include VPC and subnet resources

--- main.tf
+++ main.tf
@@ -2,6 +2,51 @@ provider "aws" {
   region = var.aws_region
 }
 
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
+  availability_zone = var.availability_zone
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
+resource "aws_security_group" "main" {
+  name        = var.sg_name
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = var.sg_name
+  }
+}
+
 resource "aws_instance" "Jenkins" {
   ami               = var.ami_id
   subnet_id         = var.subnet_id
@@ -13,3 +58,4 @@ resource "aws_instance" "Jenkins" {
     Name = var.instance_name
   }
 }
+
