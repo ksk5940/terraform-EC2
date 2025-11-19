e829a66cfc6e4ca9635c707f2b41c26f6ff00a09
Sreekanth K <ksk5940@gmail.com>
2025-11-19 16:11:58 +0530

    Refactor outputs for EC2 and VPC resources
    Removed outputs for Jenkins EC2 instance and added outputs for VPC, subnet, security group, and updated public IP output.

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,15 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.Jenkins.id
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
+
+output "sg_id" {
+  value = aws_security_group.main.id
 }
 
 output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.Jenkins.public_ip
-}
-
-output "instance_private_ip" {
-  description = "Private IP of the EC2 instance"
-  value       = aws_instance.Jenkins.private_ip
-}
-
-output "instance_arn" {
-  description = "ARN of EC2 instance"
-  value       = aws_instance.Jenkins.arn
+  value = aws_instance.ec2.public_ip
 }
